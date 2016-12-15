a6637092fd5293170fcab2c41aed091330d1ec03
Stephen Ciantar <stephen.ciantar@melitaltd.com>
2016-12-15 11:42:09 +0100

    added option to input product_number and rename the file according to product

--- get_cdrs_2.py
+++ get_cdrs_2.py
@@ -16,7 +16,8 @@ host = conf['USAGE']['host']
 port = conf['USAGE']['port']
 sid = conf['USAGE']['sid']
 
-
+product = int(input('Enter Product LOB: '))
+lob = {13:'_Voice',2:'_SMS',21:'_Data', 22:'_RC'}  
 #cdr_path = '//AMSTERDAM/billingops$/Amdocs/Operations/Billing Cycles/Cycle Files'
 cdr_path = 'E:/Call_Home_Plan/CDRs/July/Voice'
 year = int(input ('Enter Year: '))
@@ -30,22 +31,22 @@ while day <= days:
         month_b = '0'+str(month)
         if (len(str(day)) == 1):
             day_b = '0'+str(day)
-            filename = "CH_"+ day_b + "_" + str(month_b) + str(year) + ".csv"
+            filename = "CH_"+ day_b + "_" + str(month_b) + str(year) +str(lob[product])+ ".csv"
             filename = filename.replace(" ", "")
             con = CONNECTION_ORA(username,passw,host,port,sid)
             oracle = con.connect_ora()
-            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = 13 and chargeable_subs_id != -1".format(str(year), str(month_b), day_b))
+            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = {} and chargeable_subs_id != -1".format(str(year), str(month_b), day_b, product))
             prod = con.read_query(oracle, gl_query)
             gl = con.read_query(oracle, gl_query)
             gl.to_csv(filename, encoding = 'mbcs', sep =',', index = False)
             print (day_b, month_b, ' : Done!')
         else:
              
-            filename = "CH_"+ str(day) + "_" + str(month_b) + str(year) + ".csv"
+            filename = "CH_"+ str(day) + "_" + str(month_b) + str(year) +str(lob[product])+ ".csv"
             filename = filename.replace(" ", "")
             con = CONNECTION_ORA(username,passw,host,port,sid)
             oracle = con.connect_ora()
-            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = 13 and chargeable_subs_id != -1".format(str(year), str(month_b), str(day)))
+            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = {} and chargeable_subs_id != -1".format(str(year), str(month_b), str(day), product))
             prod = con.read_query(oracle, gl_query)
             gl = con.read_query(oracle, gl_query)
             gl.to_csv(filename, encoding = 'mbcs', sep =',', index = False)
@@ -53,22 +54,22 @@ while day <= days:
     else:
         if (len(str(day)) == 1):
             day_b = '0'+str(day)
-            filename = "CH_"+ day_b + "_" + str(month) + str(year) + ".csv"
+            filename = "CH_"+ day_b + "_" + str(month) + str(year) + str(lob[product])+".csv"
             filename = filename.replace(" ", "")
             con = CONNECTION_ORA(username,passw,host,port,sid)
             oracle = con.connect_ora()
-            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = 13 and chargeable_subs_id != -1".format(str(year), str(month), day_b))
+            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = {} and chargeable_subs_id != -1".format(str(year), str(month), day_b, product))
             prod = con.read_query(oracle, gl_query)
             gl = con.read_query(oracle, gl_query)
             gl.to_csv(filename, encoding = 'mbcs', sep =',', index = False)
             print (day_b, month, ' : Done!')
         else:
              
-            filename = "CH_"+ str(day) + "_" + str(month_b) + str(year) + ".csv"
+            filename = "CH_"+ str(day) + "_" + str(month_b) + str(year) +str(lob[product])+ ".csv"
             filename = filename.replace(" ", "")
             con = CONNECTION_ORA(username,passw,host,port,sid)
             oracle = con.connect_ora()
-            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = 13 and chargeable_subs_id != -1".format(str(year), str(month), str(day)))
+            gl_query =  ("Select * from USAGE.cdr_partitioned partition (P{}{}{}) where offer_id = 321 and product_type = {} and chargeable_subs_id != -1".format(str(year), str(month), str(day), product))
             prod = con.read_query(oracle, gl_query)
             gl = con.read_query(oracle, gl_query)
             gl.to_csv(filename, encoding = 'mbcs', sep =',', index = False)
